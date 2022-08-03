63f9d14aa75d99131818c620625cff48bc777b14
paha444 <itdeveloper0@gmail.com>
2022-08-03 09:54:45 +0300

    параметры
    добавил возможность вызова API с параметрами

--- app/Controllers/APIController.php
+++ app/Controllers/APIController.php
@@ -8,24 +8,60 @@ class APIController extends \MainController
 
         $data=[];
         $data['jsonrpc']='1.0';
-        $data['DateTime']=$this->getDateTime();
-        $data['UNIXTime']=$this->getUNIXTime();
+        $data['MySQLTime']=self::MySQLTime();
+        $data['UNIXTime']=self::UNIXTime();
         
         return view('/api', ['data' => $data]);
     }
     
-    private function getDateTime(){
+    public function index_v3() {
+
+        $params=\Request::get();
         
+        $data=[];
+        $data['jsonrpc']='3.0';
+        
+        foreach($params as $param){
+            if (method_exists(__CLASS__,$param)) {
+                $data[$param]=call_user_func('self::'.$param);
+            }
+        
+        }
+       
+        return view('/api', ['data' => $data]);
+    }
+    
+    
+
+    public function getMySQLTime(){
+        
+        $data=[];
+        $data['jsonrpc']='1.0';
+        $data['MySQLTime']=self::MySQLTime();
+        
+        return view('/api', ['data' => $data]);
+        
+    }
+
+    public function getUNIXTime(){
+        
+        $data=[];
+        $data['jsonrpc']='1.0';
+        $data['UNIXTime']=self::UNIXTime();
+        
+        return view('/api', ['data' => $data]);
+        
+    }
+
+
+
+    private static function MySQLTime(){
         return date('Y-m-d H:i:s');
-        
     }
 
-    private function getUNIXTime(){
-        
+    private static function UNIXTime(){
         return time();
-        
     }
-
 
 }
 
--- routes/main.php
+++ routes/main.php
@@ -12,4 +12,30 @@
 
 Route::get("/", view("default"));
 
+///////// v1
+
 Route::get("/api/v1/")->controller('APIController@index');
+Route::get("/api/v1/get_unix_time/get_mysql_time")->controller('APIController@index');
+
+Route::get("/api/v1/get_mysql_time")->controller('APIController@getMySQLTime');
+Route::get("/api/v1/get_unix_time")->controller('APIController@getUNIXTime');
+
+
+///////// v2
+
+Route::get('/api/v2/{method}/')->controller('APIController@get<method>');
+//http://hleb/api/v2/mysqltime/
+//http://hleb/api/v2/unixtime/
+
+
+
+///////// v3  можно вызывать методы в разной последовательности, главное указать имя метода
+Route::get("/api/v3/{param1}/{param2?}/")->controller('APIController@index_v3');
+//hleb/api/v3/MySQLTime/
+//hleb/api/v3/UNIXTime
+//http://hleb/api/v3/UNIXTime/MySQLTime/
+//http://hleb/api/v3/MySQLTime/UNIXTime/
+
+
+
+////////
